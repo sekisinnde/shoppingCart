8d7c54488eee7415a4987fa7a3a7dfcf7c0e86f7
oumar2 <o.sekisinde@hotmail.com>
2022-03-04 07:35:23 +0100

    premmier

--- src/main/java/com/collections/shoppingcart/Main.java
+++ src/main/java/com/collections/shoppingcart/Main.java
@@ -2,8 +2,8 @@ package com.collections.shoppingcart;
 
 public class Main {
     
-    
-        public static void main (String [] args) {
-            new UI();
-        }
+
+    public static void main (String [] args) {
+        new UI();
     }
+}
